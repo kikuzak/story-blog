01de743a7d2dbb7992154791d27c06a3d2626494
kikuzak <kazuki.kimura.1008@gmail.com>
2024-02-16 20:44:30 +0900

    エラーページの作成

---
+++ error.vue
@@ -0,0 +1,46 @@
+<template>
+    <NuxtLayout name="search">
+        <div class="article-content">
+            <p>お探しのページは見つかりませんでした。</p>
+        </div>
+
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: `MARINTO GOA | エラー`,
+});
+
+</script>
+
+<style scoped lang="scss">
+header {
+    margin-block-end: 1rem;
+
+    h1 {
+        margin-block-end: 0.2rem;
+    }
+}
+.article-content {
+    padding-block: 1rem;
+    @include mq(pc) {
+        padding-block-start: 0;
+    }
+}
+
+.article-text {
+    padding-bottom: 1rem;
+}
+
+.article-attribute-list {
+    justify-content: left;
+    flex-wrap: wrap;
+}
+
+.article-attribute-item {
+    font-size: 0.7rem;
+    margin-block-end: 0;
+    margin-inline-end: 1rem;
+}
+</style>
