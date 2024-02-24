d063ff62a78f9e8c65f2d460306be3c6ee01c046
kikuzak <kazuki.kimura.1008@gmail.com>
2024-02-24 14:32:06 +0900

    favicon・OGPの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,25 +12,22 @@ export default defineNuxtConfig({
         {charset: 'utf-8'},
         {property: 'og:site_name', content: 'MARINTO GOA (マリントゴア)'},
         {property: 'og:type', content: 'article'},
-        {property: 'og:image', content: 'https://marintogoa.net/ogp.png'},
+        {property: 'og:image', content: '/ogp.png'},
         
       ],
       link: [
-        {
-          rel: "preconnect",
-          href: "https://fonts.googleapis.com"
-        },
-        {
-          rel: "stylesheet",
-          href: "https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-        }
+        {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+        {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png'},
+        {rel: 'icon', type: 'image/png', sizes: '192x192', href: '/icon-192x192.png'},
+        {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+        {rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.15.4/css/all.css'}
       ]
     }
   },
   css: [
-    "@/assets/css/reset.css",
-    "@/assets/css/k-framework.scss",
-    "@/assets/css/article.scss"
+    '@/assets/css/reset.css',
+    '@/assets/css/k-framework.scss',
+    '@/assets/css/article.scss'
   ],
   gtag: {
     id: 'G-K4GW1V0ZRM'
